fdc974dbac7a55787be1e6dfe89ffed2aa915f20
George Dernikos <geoder101@gmail.com>
2014-12-14 03:59:47 +0200

    add setup.py
    (egg packaging)

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='py_shell',
+    version='0.1',
+    license="MIT",
+    author="George Dernikos",
+    author_email="geoder101@gmail.com",
+    description="Handy functions to execute external shell commands",
+    url="https://github.com/geoder101/py_shell",
+    packages=find_packages()
+)
